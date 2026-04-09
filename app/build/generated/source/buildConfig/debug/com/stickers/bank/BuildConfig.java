b44429c88c5d32a5d70b0b79996707818ecff101
UtkarshCodeCom <yushkarsh@gmail.com>
2026-04-09 23:51:49 +0530

    1.1

---
+++ app/build/generated/source/buildConfig/debug/com/stickers/bank/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.stickers.bank;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.stickers.bank";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 18;
+  public static final String VERSION_NAME = "5.5";
+  // Field from build type: debug
+  public static final String ADV_AOIDS = "ca-app-pub-3940256099942544/3419835294";
+  // Field from build type: debug
+  public static final String ADV_BIDS = "ca-app-pub-3940256099942544/6300978111";
+  // Field from build type: debug
+  public static final String ADV_IIDS = "ca-app-pub-3940256099942544/1033173712";
+  // Field from build type: debug
+  public static final String ADV_NAIDS = "ca-app-pub-3940256099942544/2247696110";
+  // Field from default config.
+  public static final String CONTENT_PROVIDER_AUTHORITY = "com.stickers.bank.provider.StickerContentProvider";
+  // Field from default config.
+  public static final String CONTENT_PROVIDER_AUTHORITY_API = "com.stickers.bank.stickercontentproviderapi";
+}
